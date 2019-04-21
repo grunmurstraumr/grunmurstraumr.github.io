8ac5f947af18bd11e849bef7743b3e33c0cf6957
Erik <erik.grundstrom@hotmail.com>
2019-04-21 13:34:06 +0200

    Fully featured skills section

--- js/main_new.js
+++ js/main_new.js
@@ -1,4 +1,8 @@
-
+clear_node = node =>{
+    while (node.firstChild){
+        node.removeChild(node.firstChild);
+    }
+}
 class Output{
     // This class manages writing to an html element in a typing style. A class is used in order to ensure that
     // two texts are not written to the element at the same time due to the asynchronous nature of the setInterval
@@ -9,161 +13,190 @@ class Output{
         if (this.html_node === undefined || this.html_node === null)
             throw(`DOM error. No node with '${node_id}' found.`)
         this.queue = []
-        this.initial_value = this.html_node.innerHTML;
         this.writing = false;
         this.max_queue = max_queue;
-        this.speed_modifier = 100;
+        this.speed_modifier = document.querySelector('#typing_speed_control').value;
         this.cancel = false;
-        this.caret = '<span class="caret">\u2588</span>';
+        this.caret ='<span class="caret">\u2588</span>';
+        this.pause_length = 500;
     } // constructor end
 
     set typing_speed(new_val){
         this.speed_modifier = new_val;
     }
-    _parse_tags(text){
-        
-    }
+
     _inner_write(text, text_speed){
         // This function actually writes to the output element defined in the constructor.
-        
         // Check if writing flag is active. If so, cancel write
         if (this.writing){
             return
         }
+        // Mark writing flag
         this.writing = true
-        // Reset html to initial value, definied in html
-        this.html_node.innerHTML = this.initial_value;
 
         // Index and trail are necessary outside of setInterval scope in order to refer to them
         // on each iteration
-        let index = 0;
-        let trail = "";
-        let tag = "";
-        let inside_tag = "";
-
+        let index=0;
+        let iteration_delay = this.speed_modifier
+        clear_node(this.html_node);
+        /*while(this.html_node.firstChild){
+            this.html_node.removeChild(this.html_node.firstChild);
+        }*/
+        let output_wrapper = this.html_node;
         // Start writing to the output html element
         // The function forms a closure which is needed for the async execution
         const write_to_node = () =>{
-            // Remove trailing tags and caret.
-            //console.log(`Tag: ${tag}\nTrail: ${trail}\nInside: ${inside_tag}`);
             if (this.speed_modifier <= 0){
                 this.html_node.innerHTML = text + this.caret;
+                let navlinks = this.html_node.querySelectorAll('.section_navlink');
+                console.log(navlinks)
+                let nav_container = document.querySelector('#secondary_nav');
+                clear_node(nav_container);
+                for(let node of navlinks){
+                    node.parentNode.removeChild(node);
+                    nav_container.appendChild(node);
+                }
                 this.writing = false;
                 return;
             }
-            if (trail !== ""){
-                this.html_node.innerHTML = this.html_node.innerHTML.slice(0, (this.html_node.innerHTML.length-(tag.length+inside_tag.length+trail.length+this.caret.length)));
+            if (this.cancel){
+                this.writing = false;
+                this.cancel = false;
+                if(this.queue.length > 0){
+                    let next = this.queue.shift();
+                    this._inner_write(next.text)
+                }
+                return
             }
-            else{
-                this.html_node.innerHTML = this.html_node.innerHTML.slice(0,this.html_node.innerHTML.length-this.caret.length);
+            try{
+                if (output_wrapper.lastChild.classList.contains('caret')){
+                    output_wrapper.removeChild(output_wrapper.lastChild);
+                }
+            } catch{
+                
             }
             let next_char = text[index];
-            // First check for special cases:
-            // replace newlines with spaces  
-            /*          
-            if (next_char === "\n"){
-                next_char = " ";
-            }
-            */
-            console.log(`Next char is: ${next_char}`);
-            //console.log(`index is : ${index}`);
-
-            // If the character is <, it is the start of a html tag. These are allowed and we must therefore
-            // iterate through and collect all characters until we reach the > that closes the tag.
+            let start_index = 0;
             if (next_char === '<'){
-                do{
-                    ++index
-                    next_char += text[index];
-                } while(text[index] != '>')
-                //Check if this is a closing tag, if not we must assign corresponding closing tag as a trailing tag, otherwise
-                // we weill get automatic closing of the tag.
-                if(next_char[1] !== '/'){
+                let block_append = false;
+                let old_index = index;
+                // Find the end of the tag
+                index = text.indexOf('>', index);
+                let tag = text.substring(old_index, index+1)
+                console.log(`Found tag: ${tag}`)
+                console.log(output_wrapper)
+                if (tag.match(/<\w+[\s\w=#_"+'.:/\-]*\/?>/)){
+                    // Tag is an opening tag
+                    // Check for details tag
+                    if (tag.match(/<li.*>/)){
+                        // If details tag is matched, find the closing tag
+                        index = text.indexOf('</li>',index)
+                        // Find the index of the last > of the tag
+                        index = text.indexOf('>', index);
+                        // Collect the entire contents of the details tag
+                        tag = text.substring(old_index, index+1);
+                        iteration_delay = 250;
+                        
+                    } else if(tag.match(/<details.*>/)){
+                        index = text.indexOf('</details>',index)
+                        // Find the index of the last > of the tag
+                        index = text.indexOf('>', index);
+                        // Collect the entire contents of the details tag
+                        tag = text.substring(old_index, index+1);
+                        iteration_delay = 250;
                     
-                    inside_tag ="";
-                    tag += next_char;
-                    trail += next_char.splice(1,0, '/');
-                    let white_space_index = trail.indexOf(' ');
-                    // Remove anything inside the tag trailing a space, otherwise classes end up in closing tag as well
-                    if (white_space_index > 0){
-                        trail = trail.splice(white_space_index, trail.length-white_space_index-1,'');
+                    }else if(tag.match(/<a\b.*>/)){
+                        index = text.indexOf('</a>', index);
+                        index = text.indexOf('>', index);
+                        tag = text.substring(old_index,index+1);
+                        if (tag.match(/class=\s?["']{1}section_navlink["']{1}/)){
+                            console.log('Found navlink')
+                            iteration_delay=0;
+                            let nav_element = document.createRange().createContextualFragment(tag);
+                            let nav_container = document.querySelector('#secondary_nav');
+                            
+                            nav_container.appendChild(nav_element);
+                            block_append=true;
+                        }
                     }
-                    next_char="";
+                    else
+                        iteration_delay = 0;
+                    if (!block_append){
+                        let temp = document.createRange().createContextualFragment(tag);
+                        output_wrapper.appendChild(temp);
+                    }
+                    if(!tag.match(/<img[\D\d\s]*>/) && !block_append){
+                        // Set new node as output node if it is not an image. Currently only images are
+                        // supported as single tag elements
+                        output_wrapper = output_wrapper.lastChild;
+                    }
+                        
+                } else if(tag.match(/<\/\w+[\s\w=#_+"'.:/\-]*>/)){
+                    //Closing tag
+                    output_wrapper = output_wrapper.parentElement;
+                }else if(tag.match(/<!\-\-.*\-\->/)){
+                    // Matching a html comment do nothing
+                } else{
+                    console.error(`Invalid tag matched: ${tag}`)
+                    throw('Parse error');
+                    
                 }
-                else{
-                    console.log('Tag else');
-                    next_char = `${tag}${inside_tag}${trail}`;
-                    tag = "";
-                    trail ="";
-                    inside_tag ="";
-                }
-                //console.log(`next_char ${next_char} and trail ${trail}`)
-                
             }
-            // If character to be written is a space, skip one iteration and print both the space
-            // and the next character. A check is made to ensure that the next character is actually valid
-            else if (next_char === ' ' && text[index+1] !== undefined){
-                // Since we skip an iteration we need to increment index twice, first here and second time at the end of the function
-                
-                while(text[index+1] === ' '){
-                    ++index;
-                }
-                
-                //++index;
-                next_char += text[index]
-                // This condition is special, browser may collapse the leading space if we do not do
-                //this.html_node.innerHTML += ' ' + 
+            else{
+                iteration_delay = this.speed_modifier;
+                if(!output_wrapper.classList.contains('no_type')) // This if prevents the image from "blinking" because of re-rendering of the base node
+                    output_wrapper.innerHTML += next_char + this.caret;
             }
-            console.log(`Before adding inside tag: ${next_char}`)
-            if (tag !== ""){
-                inside_tag += next_char;
-                next_char = inside_tag;
+            if(index < 0){
+                console.log('Somethings wrong!')
+                return
             }
-            //console.log(`Output: ${tag}${next_char}${this.caret}${trail}`)
-            //console.log(`Node text: \n ${this.html_node.innerHTML}`)
-            console.log(`Wrote ${next_char.length} charaters`);
-            console.log(next_char);
-            this.html_node.innerHTML += `${tag}${next_char}${this.caret}${trail}`;
+            index++;
             
-            ++index;
-
-            // Check whether there are more iterations to be made, if we printed the last character
-            // clear the interval, toggle css-classes, toggle writing flag and check if there is 
-            // new text to be written in the queue.
-            if (index >= text.length || this.cancel){
-                //clearInterval(this.interval);
-                this.cancel = false;
-                this.writing = false;
-                if (this.queue.length > 0){
-                    // This is executed if text is waiting in the queue. Set a timeout for the call to
-                    // _inner_write to have a delay before the next texts is written.
-                    let call = this.queue.shift();
-                    this._inner_write(call.text, call.text_speed);
-                    return;
-                }
+            if (index < text.length){
+                setTimeout(write_to_node, iteration_delay);
             }
-            else 
-                setTimeout(write_to_node, this.speed_modifier);
-        };// end function def, this.speed_modifier); // end interval
-        setTimeout(write_to_node, this.speed_modifier)
-    } //end method _inner_write
-
+            else{
+                //Insert the final caret
+                output_wrapper.lastChild.innerHTML += this.caret;
+                this.writing = false;
+                return
+            }
+        }
+        write_to_node();
+    }
+            
     write(text, text_speed){
         // write checks the writing flag and if something is already being written to the output
         // it stores the new write command in it's internal queue, provided it's not at capacity of course.
         // If output is currently not being written to, automatically starts writing to the output.
         let out_text = text.trim().replace(/[\n ]{1,}/g, " ");
-        console.log(out_text);
+        if (out_text[0] !== "<"){
+            // Ensure that input text is wrapped in a html tag
+            out_text = `<span>${out_text}</span>`
+        }
         if (this.writing){
             this.cancel = true;
             this.queue.push({'text': out_text, 'text_speed': text_speed});
         }
-        else{
-            this._inner_write(text, text_speed);
-        }
+        else 
+            this._inner_write(out_text, text_speed);
+
     } // write method end
 } // class Output definition end
 
+const scroll_handler = event => {
+    // Check for secondary navbar position
+    let secondary_nav = document.querySelector('#secondary_nav');
+    let navbar_pos = secondary_nav.offsetTop;
+    if (window.pageYOffset >= navbar_pos ){ //&& navbar_pos.firstChild){
+        secondary_nav.classList.add('sticky')
+    }
+    else{
+        secondary_nav.classList.remove('sticky')
+    }
 
+}
 
 // Global variables
 let initial_state = true;
@@ -173,27 +206,30 @@ function write(text, text_speed=500){
 }
 
 function assign_listeners(){
+    let section_links = document.querySelectorAll('.section_select');
+    section_links.forEach(element => element.addEventListener('click', event=>{
+        // Turns of initial state animations such as blinking links
+        initial_state = false;
+        clear_node(document.querySelector('#secondary_nav'))
+    }));
     document.querySelector('#introduction_link').addEventListener('click',(event) => {
-        //Check if app is in initial state, if so toggle it out of the initial state
-        if (initial_state){
-            initial_state = false;
-            //document.querySelector('#cmd_out').classList.remove('caret_pos', 'caret_blink');
-        }
         let intro = document.querySelector('#introduction');
-        write(intro.innerHTML.trim());
-        //tw.deleteAll(0);
-        //tw.typeString(intro.innerHTML.trim().replace(/ {1,}/g, " ")).start()
+        write(intro.innerHTML);
     });
-    document.querySelector('#b').addEventListener('click',(event) => {
-        write("This is text B.<p>New paragraph</p><h1><span>And a heading!</span></h1>", 50);
+    document.querySelector('#skills_link').addEventListener('click',(event) => {
+        let skills_section = document.querySelector('#skills');
+        write(skills_section.innerHTML);
     });
     document.querySelector('#c').addEventListener('click',(event) => {
-        write("This is text C.");
+        let cv_section = document.querySelector('#cv');
+        write(cv_section.innerHTML);
     });
     
     document.querySelector('#typing_speed_control').addEventListener('change', event =>{
         output.typing_speed = event.target.value;
     })
+
+    window.addEventListener('scroll', scroll_handler);
 
 }
 let tw;
@@ -223,8 +259,9 @@ document.addEventListener('DOMContentLoaded', () =>{
             i = 0;
 
     },500);
-    output.write("Hej Tromb!\nKlicka på länkarna ovanför för att se min ansökan!");
+    output.write("Hej <strong>Tromb!</strong> <pause>\nKlicka på länkarna ovanför för att se min ansökan!");
     let out = document.querySelector('#std_out_wrapper');
     //console.log(out);
     //tw = new Typewriter(out,{delay: 1, cursor: '\u2588'});
 })
+
